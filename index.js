282e9f6e6272d98b64d914524d0a7a68f97083ca
Md Saiful Haque <saifulhaque.pciu@gmail.com>
2025-11-09 12:01:39 +0600

    mongoDB connected successfully

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+const uri = "mongodb+srv://farmer-db:mFlezlHmNcgnpP6Q@cluster0.nmodl3i.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Server is running')
 })
+
+async function run() {
+  try {
+    await client.connect();
+    
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
